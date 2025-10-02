d8e0a31bc8ecd58717ac7cf72944a30b690467d5
Rio Wickham (20112490) <wickham.rio@gmail.com>
2025-10-02 13:53:26 +0100

    Write skeleton menu for Notes App

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,58 @@
 package ie.setu
+import java.lang.System.exit
 
 fun main() {
-    println("Hello World!")
+    runMenu()
+}
+
+fun mainMenu() : Int {
+    print("""
+          > ----------------------------------
+          > |        NOTE KEEPER APP         |
+          > ----------------------------------
+          > | NOTE MENU                      |
+          > |   1) Add a note                |
+          > |   2) List all notes            |
+          > |   3) Update a note             |
+          > |   4) Delete a note             |
+          > ----------------------------------
+          > |   0) Exit                      |
+          > ----------------------------------
+          > ==>> """.trimMargin(">"))
+    return readln().toInt()
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1 -> addNote()
+            2 -> listNotes()
+            3 -> updateNote()
+            4 -> deleteNote()
+            0 -> exitApp()
+            else -> println("Invalid option entered: $option")
+        }
+    } while (true)
+}
+
+fun addNote() {
+    println("You chose Add Note")
+}
+
+fun listNotes() {
+    println("You chose List Notes")
+}
+
+fun updateNote() {
+    println("You chose Update Note")
+}
+
+fun deleteNote() {
+    println("You chose Delete Note")
+}
+
+fun exitApp() {
+    println("Exiting.. bye!")
+    exit(0)
 }
